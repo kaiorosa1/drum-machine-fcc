a0a3ce70c7810cc4fbb9499ed3ddfc4f6a6f92fa
kaiorosa1 <kaiosrosa@gmail.com>
2019-05-18 12:56:22 -0300

    add drum pad

--- js/index.js
+++ js/index.js
@@ -2,7 +2,30 @@ class App extends React.Component {
   render() {
     return (
       React.createElement("div", null,
-      React.createElement("h1", null, "App Component")));
+      React.createElement("h1", null, "App Component"),
+      React.createElement(DrumMachine, null)));
+
+
+  }}
+
+
+class DrumMachine extends React.Component {
+  render() {
+    return (
+      React.createElement("div", { id: "drum-machine" },
+      React.createElement("h2", null, "Drum Component"),
+      React.createElement("div", { id: "display" }),
+      React.createElement("div", { class: "container" },
+      React.createElement("div", { class: "drum-pad", id: "Q" }, "Q"),
+      React.createElement("div", { class: "drum-pad", id: "W" }, "W"),
+      React.createElement("div", { class: "drum-pad", id: "E" }, "E"),
+      React.createElement("div", { class: "drum-pad", id: "A" }, "A"),
+      React.createElement("div", { class: "drum-pad", id: "S" }, "S"),
+      React.createElement("div", { class: "drum-pad", id: "D" }, "D"),
+      React.createElement("div", { class: "drum-pad", id: "Z" }, "Z"),
+      React.createElement("div", { class: "drum-pad", id: "X" }, "X"),
+      React.createElement("div", { class: "drum-pad", id: "C" }, "C"))));
+
 
 
   }}
