cd195d4b9437d8b63831c3a680f56671afa6ea66
kaiorosa1 <kaiosrosa@gmail.com>
2019-05-19 13:10:39 -0300

    add display changing

--- js/index.js
+++ js/index.js
@@ -15,13 +15,23 @@ class App extends React.Component {
 class DrumMachine extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      displayText: 'default text' };
+
   }
+
+  changeDisplay(txt) {
+    this.setState({
+      displayText: txt });
+
+  }
+
   render() {
     return (
       React.createElement("div", { id: "drum-machine" },
       React.createElement("h2", null, "Drum Component"),
-      React.createElement(Display, null),
-      React.createElement(DrumPad, null)));
+      React.createElement(Display, { displayText: this.state.displayText }),
+      React.createElement(DrumPad, { changeDisplay: this.changeDisplay.bind(this) })));
 
 
   }}
@@ -30,14 +40,11 @@ class DrumMachine extends React.Component {
 class Display extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      text: 'display default text' };
-
   }
   render() {
     return (
       React.createElement("div", null,
-      React.createElement("div", { id: "display" }, this.state.text)));
+      React.createElement("div", { id: "display" }, this.props.displayText)));
 
 
   }}
@@ -48,21 +55,22 @@ class DrumPad extends React.Component {
     super(props);
   }
   keyClicked(e) {
-    alert("The " + e.target.id + " was clicked");
+    this.props.changeDisplay(e.target.id);
+    // depending on the id play a sound
   }
   render() {
     return (
       React.createElement("div", null,
       React.createElement("div", { class: "container" },
-      React.createElement("div", { class: "drum-pad", id: "Q", onClick: this.keyClicked }, "Q"),
-      React.createElement("div", { class: "drum-pad", id: "W", onClick: this.keyClicked }, "W"),
-      React.createElement("div", { class: "drum-pad", id: "E", onClick: this.keyClicked }, "E"),
-      React.createElement("div", { class: "drum-pad", id: "A", onClick: this.keyClicked }, "A"),
-      React.createElement("div", { class: "drum-pad", id: "S", onClick: this.keyClicked }, "S"),
-      React.createElement("div", { class: "drum-pad", id: "D", onClick: this.keyClicked }, "D"),
-      React.createElement("div", { class: "drum-pad", id: "Z", onClick: this.keyClicked }, "Z"),
-      React.createElement("div", { class: "drum-pad", id: "X", onClick: this.keyClicked }, "X"),
-      React.createElement("div", { class: "drum-pad", id: "C", onClick: this.keyClicked }, "C"))));
+      React.createElement("div", { class: "drum-pad", id: "Q", onClick: this.keyClicked.bind(this) }, "Q"),
+      React.createElement("div", { class: "drum-pad", id: "W", onClick: this.keyClicked.bind(this) }, "W"),
+      React.createElement("div", { class: "drum-pad", id: "E", onClick: this.keyClicked.bind(this) }, "E"),
+      React.createElement("div", { class: "drum-pad", id: "A", onClick: this.keyClicked.bind(this) }, "A"),
+      React.createElement("div", { class: "drum-pad", id: "S", onClick: this.keyClicked.bind(this) }, "S"),
+      React.createElement("div", { class: "drum-pad", id: "D", onClick: this.keyClicked.bind(this) }, "D"),
+      React.createElement("div", { class: "drum-pad", id: "Z", onClick: this.keyClicked.bind(this) }, "Z"),
+      React.createElement("div", { class: "drum-pad", id: "X", onClick: this.keyClicked.bind(this) }, "X"),
+      React.createElement("div", { class: "drum-pad", id: "C", onClick: this.keyClicked.bind(this) }, "C"))));
 
 
 
